d962d6a9095604f5265b5bd9527113ad2d52306e
William Ray Johnson <willrayjohnson@gmail.com>
2017-10-16 15:10:50 -0500

    Modified to allow for n distance between number per the bonus of challenge

--- challenges/335[easy]/consecutiveDistance.py
+++ challenges/335[easy]/consecutiveDistance.py
@@ -2,14 +2,14 @@
 https://www.reddit.com/r/dailyprogrammer/comments/759fha/20171009_challenge_335_easy_consecutive_distance/
 '''
 
-def calcuateConsecutiveDist(listInput):
+def calcuateConsecutiveNDist(listInput, n):
     sortedList = list(listInput)
     sortedList.sort()
     consectDistRating = 0
     
     for item in range(len(sortedList) - 1):
-        consecutiveValue = sortedList[item] + 1
-        if consecutiveValue == sortedList[item + 1]:
-            consectDistRating += abs(listInput.index(sortedList[item]) - listInput.index(sortedList[item + 1]))
+        consecutiveValue = sortedList[item] + n
+        if consecutiveValue in sortedList:
+            consectDistRating += abs(listInput.index(sortedList[item]) - listInput.index(consecutiveValue))
     
     return consectDistRating
--- challenges/335[easy]/consecutiveDistanceTest.py
+++ challenges/335[easy]/consecutiveDistanceTest.py
@@ -6,49 +6,56 @@ class consecutiveDistanceTest(unittest.TestCase):
     def testExampleInput1(self):
         listInput = [6, 11]
         expectedOutput = 0
-        actualOutput = cs.calcuateConsecutiveDist(listInput)
+        actualOutput = cs.calcuateConsecutiveNDist(listInput, 1)
         
         self.assertEqual(expectedOutput, actualOutput)
         
     def testExampleInput2(self):
         listInput = [31,63,53,56,96,62,73,25,54,55,64]
         expectedOutput = 26
-        actualOutput = cs.calcuateConsecutiveDist(listInput)
+        actualOutput = cs.calcuateConsecutiveNDist(listInput, 1)
         
         self.assertEqual(expectedOutput, actualOutput)
         
     def testExampleInput3(self):
         listInput = [77, 39, 35, 38, 41, 42, 76, 73, 40, 31, 10]
         expectedOutput = 20
-        actualOutput = cs.calcuateConsecutiveDist(listInput)
+        actualOutput = cs.calcuateConsecutiveNDist(listInput, 1)
         
         self.assertEqual(expectedOutput, actualOutput)
         
     def testExampleInput4(self):
         listInput = [30, 63, 57, 87, 37, 31, 58, 83, 34, 76, 38]
         expectedOutput = 15
-        actualOutput = cs.calcuateConsecutiveDist(listInput)
+        actualOutput = cs.calcuateConsecutiveNDist(listInput, 1)
         
         self.assertEqual(expectedOutput, actualOutput)
         
     def testExampleInput5(self):
         listInput = [18, 62, 55, 92, 88, 57, 90, 10, 11, 96, 12]
         expectedOutput = 3
-        actualOutput = cs.calcuateConsecutiveDist(listInput)
+        actualOutput = cs.calcuateConsecutiveNDist(listInput, 1)
         
         self.assertEqual(expectedOutput, actualOutput)
         
     def testExampleInput6(self):
         listInput = [26, 8, 7, 25, 52, 17, 45, 64, 11, 35, 12]
         expectedOutput = 6
-        actualOutput = cs.calcuateConsecutiveDist(listInput)
+        actualOutput = cs.calcuateConsecutiveNDist(listInput, 1)
         
         self.assertEqual(expectedOutput, actualOutput)
         
     def testExampleInput6(self):
         listInput = [89, 57, 21, 55, 56, 81, 54, 100, 22, 62, 50]
         expectedOutput = 13
-        actualOutput = cs.calcuateConsecutiveDist(listInput)
+        actualOutput = cs.calcuateConsecutiveNDist(listInput, 1)
+        
+        self.assertEqual(expectedOutput, actualOutput)
+        
+    def testRatingWithDistOf2(self):
+        listInput = [3, 9, 20, 11, 1, 7]
+        expectedOutput = 10
+        actualOutput = cs.calcuateConsecutiveNDist(listInput, 2)
         
         self.assertEqual(expectedOutput, actualOutput)
 if __name__ == '__main__':
