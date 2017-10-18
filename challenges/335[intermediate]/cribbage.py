18ed393c2c426f8472a17c48f80f7cac14aff765
William Ray Johnson <willrayjohnson@gmail.com>
2017-10-17 23:53:48 -0500

    Card Values

--- challenges/335[intermediate]/cribbage.py
+++ challenges/335[intermediate]/cribbage.py
@@ -2,5 +2,7 @@
 https://www.reddit.com/r/dailyprogrammer/comments/75p1cs/20171011_challenge_335_intermediate_scoring_a/
 '''
 
+cardValues = ('0','A','2','3','4','5','6','7','8','9','10','J','Q','K')
+
 def scoreHand(hand):
     return None
