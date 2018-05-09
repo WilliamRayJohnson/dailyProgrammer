c9bcb4be53c4ec522ce0ee05323606a1b9d43ae8
William Ray Johnson <willrayjohnson@gmail.com>
2018-05-08 20:08:23 -0500

    Setup for challenge 359[hard]

---
+++ challenges/359[hard]/gridPrimes.c
@@ -0,0 +1,13 @@
+/**
+ * A pthreads program that finds all distinict primes embedded in a square
+ * matrix
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+int main(int argc, char *argv[]) {
+
+    exit(0);
+}
