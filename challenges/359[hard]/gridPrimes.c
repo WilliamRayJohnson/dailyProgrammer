0f009214ced723cd3eb79f62e1f59a5e5ffe2d41
William Ray Johnson <willrayjohnson@gmail.com>
2018-05-08 23:05:42 -0500

    Added thread creation for rows and columns

--- challenges/359[hard]/gridPrimes.c
+++ challenges/359[hard]/gridPrimes.c
@@ -7,12 +7,22 @@
 #include <stdlib.h>
 #include <pthread.h>
 
+typedef struct grid_thread_data {
+    int xOne, yOne;
+    int xTwo, yTwo;
+    int thdId;
+    int lineType;
+} Grid_Thread_Data;
+
+void *processGridLine(void *arg);
+
 char **grid;
 
 int main(int argc, char *argv[]) {
     int gridSize;
     FILE *fp;
     int i, j;
+    int threadCount = 0;
 
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <grid_file>\n", argv[0]);
@@ -23,7 +33,6 @@ int main(int argc, char *argv[]) {
     // Get grid size
     fscanf(fp, "%d", &gridSize);
     fgetc(fp);
-    printf("Grid size: %d\n", gridSize);
 
     // Allocate memory for grid
     grid = (char **) malloc(gridSize * sizeof(char *));
@@ -37,5 +46,69 @@ int main(int argc, char *argv[]) {
         fgetc(fp);
     }
 
+    // Create threads for rows
+    pthread_t *rowThreads[gridSize];
+    Grid_Thread_Data *rowThdInput;
+    for (i = 0; i < gridSize; i++) {
+        rowThdInput = (Grid_Thread_Data *) malloc(sizeof(Grid_Thread_Data));
+        rowThdInput->xOne = i;
+        rowThdInput->yOne = 0;
+        rowThdInput->xTwo = i;
+        rowThdInput->yTwo = gridSize - 1;
+        rowThdInput->lineType = 0;
+        rowThdInput->thdId = threadCount;
+        rowThreads[i] = (pthread_t *) malloc(sizeof(pthread_t));
+        if (pthread_create(rowThreads[i], NULL,
+                    processGridLine, (void *) rowThdInput)) {
+            fprintf(stderr, "Error creating row thread %d", i);
+            exit(-1);
+        }
+        threadCount++;
+    }
+
+    // Create threads for columns
+    pthread_t *colThreads[gridSize];
+    Grid_Thread_Data *colThdInput;
+    for (i = 0; i < gridSize; i++) {
+        colThdInput = (Grid_Thread_Data *) malloc(sizeof(Grid_Thread_Data));
+        colThdInput->xOne = 0;
+        colThdInput->yOne = i;
+        colThdInput->xTwo = gridSize - 1;
+        colThdInput->yTwo = i;
+        colThdInput->lineType = 1;
+        colThdInput->thdId = threadCount;
+        colThreads[i] = (pthread_t *) malloc(sizeof(pthread_t));
+        if (pthread_create(colThreads[i], NULL,
+                    processGridLine, (void *) colThdInput)) {
+            fprintf(stderr, "Error creating col thread %d", i);
+            exit(-1);
+        }
+        threadCount++;
+    }
+
+    // Join row threads
+    for (i = 0; i < gridSize; i++) {
+        if (pthread_join(*rowThreads[i], NULL)) {
+            fprintf(stderr, "Error joining row thread %d", i);
+            exit(-1);
+        }
+    }
+
+    // Join col threads
+    for (i = 0; i < gridSize; i++) {
+        if (pthread_join(*colThreads[i], NULL)) {
+            fprintf(stderr, "Error joining col thread %d", i);
+            exit(-1);
+        }
+    }
+
     exit(0);
+}
+
+void *processGridLine(void *arg) {
+    Grid_Thread_Data *gridData = (Grid_Thread_Data *) arg;
+    printf("Type %d thread created: [%d, %d] -> [%d, %d]\n",
+            gridData->lineType, gridData->xOne, gridData->yOne,
+            gridData->xTwo, gridData->yTwo);
+    pthread_exit(NULL);
 }
